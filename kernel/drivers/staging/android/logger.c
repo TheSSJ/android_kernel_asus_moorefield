9e301344206e43e1cd262e6adc09caa4cae386d2
AndiP <andreasp@gmx.de>
2015-09-04 19:00:44 +0200

    Add Logger_Interface
    thanks to @andip71

--- kernel/drivers/staging/android/logger.c
+++ kernel/drivers/staging/android/logger.c
@@ -30,7 +30,7 @@
 #include <linux/vmalloc.h>
 #include <linux/aio.h>
 #include "logger.h"
-
+#include "logger_interface.h"
 #include <asm/ioctls.h>
 
 /**
@@ -419,7 +419,8 @@ static void fix_up_readers(struct logger_log *log, size_t len)
 static void do_write_log(struct logger_log *log, const void *buf, size_t count)
 {
 	size_t len;
-
+	if (logger_mode == 0)
+		return;
 	len = min(count, log->size - log->w_off);
 	memcpy(log->buffer + log->w_off, buf, len);
 
@@ -442,7 +443,8 @@ static ssize_t do_write_log_from_user(struct logger_log *log,
 				      const void __user *buf, size_t count)
 {
 	size_t len;
-
+	if (logger_mode == 0)
+		return 0;
 	len = min(count, log->size - log->w_off);
 	if (len && copy_from_user(log->buffer + log->w_off, buf, len))
 		return -EFAULT;
